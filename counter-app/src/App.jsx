b09509d87161edb4f3bcefe1e2716814bdf07784
mahnoorsiddiqui599 <mahnoorsiddiqui599@gmail.com>
2025-12-18 21:40:49 +0500

    first commit

---
+++ counter-app/src/App.jsx
@@ -0,0 +1,32 @@
+import { useState } from "react";
+import "./App.css";
+
+export default function Counter() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="main">
+      <div className="card">
+        <h2>Counter App</h2>
+
+        <div className="count-box">
+          {count}
+        </div>
+
+        <div className="buttons">
+          <button className="btn minus" onClick={() => setCount(count - 1)}>
+            −
+          </button>
+
+          <button className="btn reset" onClick={() => setCount(0)}>
+            Reset
+          </button>
+
+          <button className="btn plus" onClick={() => setCount(count + 1)}>
+            +
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
